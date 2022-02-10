f8f7c9c71e45b295de2f48c985cf0b4b89d68a4c
Semanurmutlu <58692989+semanurmutlu@users.noreply.github.com>
2022-02-10 22:41:09 +0300

    Add project files.

---
+++ ClassMetotDemo/MusteriManager.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassMetotDemo
+{
+    class MusteriManager
+    {
+       public void Ekle(Musteri musteri)
+        {
+            Console.WriteLine("Müşteri eklendi"+musteri.Id);
+        }
+
+        public void Listele(Musteri[] musteri)
+        {
+            Console.WriteLine("Müşteri Listelendi");
+            foreach (Musteri musteriler in musteri)
+            {
+                Console.WriteLine(musteriler.Id);
+            }
+        }
+        public void Sil(Musteri musteri)
+        {
+            Console.WriteLine("Müşteri silindi"+musteri.Id);
+            
+        }
+        
+    }
+}
---
+++ ClassMetotDemo/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace ClassMetotDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Musteri musteri1 = new Musteri();
+            musteri1.Id = 1;
+            musteri1.Ad = "Semanur";
+            musteri1.Soyad = "Mutlu";
+
+            Musteri musteri2 = new Musteri();
+            musteri2.Id = 2;
+            musteri2.Ad = "Esmanur";
+            musteri2.Soyad = "Mutlu";
+
+            Musteri musteri3 = new Musteri();
+            musteri3.Id = 3;
+            musteri3.Ad = "Tuba";
+            musteri3.Soyad = "Mutlu";
+
+            Musteri[] musteri = new Musteri[] { musteri1, musteri2, musteri3 };
+
+            MusteriManager musteriManager = new MusteriManager();
+            musteriManager.Ekle(musteri1);
+            musteriManager.Ekle(musteri2);
+            musteriManager.Ekle(musteri3);
+            musteriManager.Listele(musteri);
+            musteriManager.Sil(musteri1);
+            musteriManager.Sil(musteri2);
+            musteriManager.Sil(musteri3);
+        }
+    }
+}
